8ae96d5fdb586a46adff4b002eab33ef48889157
krayemnada <nadakrayem41@gmail.com>
2021-12-17 17:09:46 +0100

    second commit

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-
+import StarRating from 'react-star-rating';
 const MovieCard = ({Movie}) => {
   console.log(Movie)
 
   return (
    
     <div>
-      {/* <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src={Movie.posterUrl}/>
   <Card.Body>
     <Card.Title>{Movie.title}</Card.Title>
@@ -15,9 +15,10 @@ const MovieCard = ({Movie}) => {
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-   
+    <Card.Text><StarRating name="small-rating" caption="Small!" size={30} totalStars={5} rating={Movie.rating} />
+      </Card.Text>
   </Card.Body>
-</Card> */}
+</Card>
     </div>
   )
 }
